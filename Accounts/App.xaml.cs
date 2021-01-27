147c27fa74155224a8d56773085fe295cee2533c
Gael Girodon <contact@gaelgirodon.fr>
2021-01-27 21:58:42 +0100

    Create the project structure

---
+++ Accounts/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Accounts
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
