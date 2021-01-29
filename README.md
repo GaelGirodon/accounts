d8a20a7d34511d513cbd7ed7960ad0c00a71daa0
Gael Girodon <contact@gaelgirodon.fr>
2021-01-30 00:40:37 +0100

    Add the packaging script and improve doc

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A basic personal financial-accounting software.
 
+## Get started
+
+- Install the [.NET Desktop Runtime](https://dotnet.microsoft.com/download/dotnet/current/runtime/desktop)
+- Download the [latest release](https://github.com/GaelGirodon/accounts/releases/latest)
+- Extract the archive
+- Run `Accounts.exe`
+
 ## License
 
 **Accounts** is licensed under the GNU General Public License.
---
+++ Scripts/Package.ps1
@@ -0,0 +1,24 @@
+﻿#
+# Package.ps1
+#
+# Build and package the application.
+#
+
+$Configuration = "Release"
+$Output = ".\publish"
+$Name = "Accounts"
+
+# Clean
+if (Test-Path $Output) {
+    Remove-Item -Recurse "$Output"
+}
+
+# Build
+dotnet publish --configuration "$Configuration" --output "$Output"
+
+# Package
+Compress-Archive -Path "$Output\*" -DestinationPath "$Output\$Name.zip" -CompressionLevel "Optimal"
+
+# Checksum
+(Get-FileHash -Path "$Output\$Name.zip" -Algorithm SHA256).Hash.ToLower() + "`n" `
+ | Set-Content -Path "$Output\$Name.zip.sha256" -NoNewLine -Encoding ascii
